e486a7ba2a43e0efd7febd4284288f1aae1ef1ab
Crypt00o <octopus2672001@gmail.com>
2023-03-30 03:18:44 +0200

    add util super model ,  client model

---
+++ src/utils/client.rs
@@ -0,0 +1,27 @@
+use reqwest::{Client,ClientBuilder,header::{HeaderMap,HeaderValue,HeaderName,USER_AGENT,COOKIE,REFERER,CONTENT_TYPE}};
+
+pub fn get_client()->Client{
+    match ClientBuilder::new().build(){
+        Ok(client)=>return client,
+        Err(err)=>panic!("[-] Error While Create client : {}",err)
+    }
+}
+
+
+pub fn sis_eelu_request_headers(cookie:Option<&String>)->HeaderMap{
+    let mut headers:HeaderMap=HeaderMap::new();
+    headers.insert(USER_AGENT, HeaderValue::from_static("0xCrypt00o - EELU Moodle Login"));
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/x-www-form-urlencoded; charset=UTF-8"));
+    headers.insert(HeaderName::from_static("x-requested-with") , HeaderValue::from_static("XMLHttpRequest"));
+    headers.insert(REFERER, HeaderValue::from_static("http://sis.eelu.edu.eg/static/PortalStudent.html"));
+    match cookie {
+        Some(value)=>{
+            let boxed_cookie_str:Box<str>=value.clone().into_boxed_str();
+            let cookie_str:&str=Box::leak(boxed_cookie_str);
+            headers.insert(COOKIE, HeaderValue::from_static(cookie_str))
+        },
+        None=>None
+    };
+    return headers
+}
+
