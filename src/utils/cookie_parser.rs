af0141e65edb87b160b43c8a4c53f87471c3b982
Crypt00o <octopus2672001@gmail.com>
2023-03-30 03:20:18 +0200

    update utils super model , add cookie_parser

---
+++ src/utils/cookie_parser.rs
@@ -0,0 +1,31 @@
+use reqwest::header::HeaderMap;
+
+fn get_cookie(res_cookie_header:&str)->&str{
+    let res_cookie_header_bytes:&[u8]=res_cookie_header.as_bytes();
+    for (index,value) in res_cookie_header_bytes.iter().enumerate(){
+        if *value as char == ';' {
+            if index+1>=res_cookie_header_bytes.len(){
+                return &res_cookie_header[..index+1];
+            } 
+            else if res_cookie_header_bytes[index+1] as char ==' '{
+                return &res_cookie_header[..index+1];
+            }
+        }
+    }
+    &res_cookie_header[..]
+}
+
+pub fn parse_cookies(headers:&HeaderMap)->String{
+    let mut cookie:String=String::new();
+    for (_key,value) in headers.iter().enumerate(){
+        if value.0.as_str()=="set-cookie"{
+            if cookie.is_empty(){
+                cookie=format!("{}",get_cookie(value.1.to_str().unwrap()));
+            }
+            else{
+                cookie=format!("{} {}",cookie,get_cookie(value.1.to_str().unwrap()));
+            }
+        }
+    }
+    return cookie;   
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,2 @@
 pub mod client;
+pub mod cookie_parser;
