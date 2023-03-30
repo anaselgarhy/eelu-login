d96c31b65abaf9e8f116d57644e3f615f2ae214e
Crypt00o <octopus2672001@gmail.com>
2023-03-30 03:34:28 +0200

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+mod utils;
+mod sis;
+mod cli;
 
+
+#[tokio::main]
+async fn main()->Result<(),Box<dyn std::error::Error>>{
+   cli::cli_session::login().await;    
+   return Ok(());
 }
