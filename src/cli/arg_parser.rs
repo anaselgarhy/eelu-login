0276a9144dc0945b5bff388d5cd01cf83118df8f
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-04-12 01:16:42 +0200

    (eelu-login) Create the verbose mode, and improve the code articit
    - Related: https://github.com/Crypt00o/eelu-login/issues/8

--- src/cli/arg_parser.rs
+++ src/cli/arg_parser.rs
@@ -8,6 +8,7 @@ pub struct Arguments {
     pub password: Option<String>,
     pub user_type: Option<UserType>,
     pub open_browser: bool,
+    pub verbose: bool,
 }
 
 impl Arguments {
@@ -17,97 +18,16 @@ impl Arguments {
             password: None,
             user_type: None,
             open_browser: false,
+            verbose: false,
         }
     }
 
-    fn prompt(param_name: &str, empty_input_err: bool) -> String {
-        loop {
-            let mut prompt_buf: String = String::new();
-            print!("Enter {} : ", param_name);
-            stdout().flush().unwrap();
 
-            match stdin().read_line(&mut prompt_buf) {
-                Ok(_) => {
-                    prompt_buf.pop();
-                    if prompt_buf.is_empty() && empty_input_err {
-                        eprintln!("[-] Empty {} !", param_name);
-                        stderr().flush().unwrap();
-                        continue;
-                    } else {
-                        return prompt_buf;
-                    }
-                }
 
-                Err(error) => {
-                    eprintln!(
-                        "[-] Error While Reading {} : {} \n [!] Repeating",
-                        param_name, error
-                    );
-                    stderr().flush().unwrap();
-                    continue;
-                }
-            };
-        }
-    }
-
-    pub fn read_needed_arguments(mut self) -> Self {
-        if self.username.is_none() {
-            // Try to get username from env var
-            if let Ok(username) = std::env::var("EELU_SIS_USERNAME") {
-                self.username = Some(username);
-            } else {
-                self.username = Some(Self::prompt("Username", true));
-            }
-        }
-        if self.password.is_none() {
-            // Try to get password from env var
-            if let Ok(password) = std::env::var("EELU_SIS_PASSWORD") {
-                self.password = Some(password);
-            } else {
-                self.password = Some(Self::prompt("Password", true));
-            }
-        }
-        self
-    }
-
-    pub fn prompt_y_n(msg: &str) -> bool {
-        match Self::prompt(msg, false)
-            .to_lowercase()
-            .trim_end_matches(char::is_whitespace)
-        {
-            "y" | "yes" => true,
-            "n" | "no" => false,
-            _ => true,
-        }
-    }
-
-    pub fn prompt_enter(msg: &str) {
-        print!("{} ", msg);
-        stdout().flush().unwrap();
-        stdin().read_line(&mut String::new());
-    }
-
-    fn banner() {
-        println!(
-            r"
-
-0xCrypt00o Moodle Login - Tool for Getting Moodle Session Quickly and Login Through CLI
-    
-    Created By : Eslam Muhammad [0xCrypt00o]
-    Github Repo : https://github.com/Crypt00o/eelu-login
-    Mail : 0xCrypt00o@protonmail.com
-    Support Me On :
-        Bitcoin Address : bc1qdp3f6u3puwkyu6ztxz7hzcurd7jnjyw6hzjcas
-
-                 "
-        );
-    }
-
+    #[inline(always)]
     fn usage() {
         println!(
-            r"
-[+] Usage : eelu-login --user <username> --pass <password> --type <staff| sys-user | student >
-
+            r#"[+] Usage : eelu-login [--user <username>] --pass <password> --type <staff| sys-user | student >
 Args: 
 
 [-user | --user | --username | -username |  -u]   <username>  :  username to login with 
@@ -117,33 +37,32 @@ Args:
 usertype can be :
     [ staff | 3 ] for staff privilege
     [ sys-user | 1] for system user privilege
-    [ student | 2] for student privilege
-                 
-    "
+    [ student | 2] for student privilege"#
         );
     }
-    pub fn parse_args() -> Self {
-        Self::banner();
+
+    #[inline(always)]
+    fn parse_args(&mut self) {
         let mut cli_args: Args = args();
         let mut index: Option<String> = cli_args.next();
-        let mut parsed_arguments: Self = Self::new();
 
         loop {
             index = cli_args.next();
             if index.is_some() {
                 match index.as_ref().unwrap().as_str() {
                     "--user" | "-username" | "--username" | "-user" | "-u" => {
-                        parsed_arguments.username = cli_args.next()
+                        self.username = cli_args.next()
                     }
                     "--password" | "-password" | "--pass" | "-pass" | "-p" => {
-                        parsed_arguments.password = cli_args.next()
+                        self.password = cli_args.next()
                     }
                     "--usertype" | "-usertype" | "--type" | "-type" | "-t" => {
                         if let Some(user_type) = cli_args.next() {
-                            parsed_arguments.user_type = Some(UserType::from_string(&user_type));
+                            self.user_type = Some(UserType::from_string(&user_type));
                         }
                     }
-                    "-o" | "-open" | "--open" => parsed_arguments.open_browser = true,
+                    "-o" | "-open" | "--open" => self.open_browser = true,
+                    "-v" | "-verbose" | "--verbose" => self.verbose = true,
                     "-h" | "-help" | "--help" => {
                         Self::usage();
                         exit(0)
@@ -151,8 +70,26 @@ usertype can be :
                     _ => println!("Invalid Argument : {}", index.unwrap()),
                 }
             } else {
-                return parsed_arguments.read_needed_arguments();
+                break;
             }
         }
+    }
+
+    #[inline]
+    pub fn parse_args_and_env() -> Self {
+        let mut parsed_arguments: Self = Self::new();
+
+            // Try to get username from env var
+            if let Ok(username) = std::env::var("EELU_SIS_USERNAME") {
+                parsed_arguments.username = Some(username);
+            }
+
+            // Try to get password from env var
+            if let Ok(password) = std::env::var("EELU_SIS_PASSWORD") {
+                parsed_arguments.password = Some(password);
+            }
+
+        parsed_arguments.parse_args();
+        parsed_arguments
     }
 }
--- src/cli/cli_session.rs
+++ src/cli/cli_session.rs
@@ -1,15 +1,24 @@
 use crate::cli::arg_parser::Arguments;
 use sis_login::sis::types::user_type::UserType;
+use std::io::{stdout, stdin, Write, stderr};
 
-pub async fn login(sis: &mut sis_login::Sis<'_>) {
-    let mut args: Arguments = Arguments::parse_args();
+
+pub async fn login(sis: &mut sis_login::Sis<'_>, args: &mut Arguments) {
     let try_guess_user_type = args.user_type.is_none();
+
+    if args.verbose && try_guess_user_type {
+        println!("[+] The User Type is not specified, Trying to guess it ...");
+        println!("[!] This may take a while ...");
+    }
+
     loop {
         // add on order &slice[student , staff , system-user ] because most users of this tool will
         // be students so we start with them
         for user_type_num in [2, 3, 1] {
             if try_guess_user_type {
-                args.user_type = Some(UserType::from(user_type_num));
+                let user_type = UserType::from(user_type_num);
+                args.user_type = Some(user_type.clone());
+                if args.verbose { println!("[+] Trying login as {}", user_type.to_string()); }
             }
             // Try Login
             match sis
@@ -21,10 +30,13 @@ pub async fn login(sis: &mut sis_login::Sis<'_>) {
                 .await
             {
                 Ok(_) => {
+                    if args.verbose { println!("[+] Login Success :)"); }
                     break;
                 }
                 _ => {
-                    println!("[-] Login Faild :(");
+                    if args.verbose {
+                        println!("[-] Login Faild :(");
+                    }
                     if !try_guess_user_type {
                         break;
                     }
@@ -34,25 +46,25 @@ pub async fn login(sis: &mut sis_login::Sis<'_>) {
 
         match sis.get_moodle_session().await {
             Ok(url) => {
-                println!("[+] Moodle URL : {}", url);
+                if args.verbose { println!("[+] Moodle URL : {}", url); } else if !args.open_browser { println!("{}", url); }
 
                 // Open the Moodle URL in the default browser if the user wants to
                 if args.open_browser {
                     if let Err(err) = open::that(url) { println!("[-] Failed To Open Browser: {err}"); };
                 }
-                Arguments::prompt_enter(
-                    "\n\nPlease send blessings upon Prophet Muhammad Then Press Enter To Exit\n\n",
-                );
+                if args.verbose { prompt_enter("\n\nPlease send blessings upon Prophet Muhammad Then Press Enter To Exit\n\n"); }
+
                 return;
             }
             _ => {
-                if Arguments::prompt_y_n("[yes/no] => Do You Want to Attemp Login Again ?") {
-                    if Arguments::prompt_y_n(
+                if prompt_y_n("[yes/no] => Do You Want to Attemp Login Again ?") {
+                    if prompt_y_n(
                         "[yes/no] => Do You Want to Login Useing Same User And Pass ?",
                     ) {
                         continue;
                     } else {
-                        args = Arguments::new().read_needed_arguments();
+                        args.username = prompt("Username", true).into();
+                        args.password = prompt("Password", true).into();
                         continue;
                     }
                 } else {
@@ -61,4 +73,65 @@ pub async fn login(sis: &mut sis_login::Sis<'_>) {
             }
         }
     }
+}
+
+pub fn prompt_y_n(msg: &str) -> bool {
+    match prompt(msg, false)
+        .to_lowercase()
+        .trim_end_matches(char::is_whitespace)
+    {
+        "y" | "yes" => true,
+        "n" | "no" => false,
+        _ => true,
+    }
+}
+
+pub fn prompt_enter(msg: &str) {
+    print!("{} ", msg);
+    stdout().flush().unwrap();
+    stdin().read_line(&mut String::new());
+}
+
+#[inline]
+pub fn prompt(param_name: &str, empty_input_err: bool) -> String {
+    loop {
+        let mut prompt_buf: String = String::new();
+        print!("Enter {} : ", param_name);
+        stdout().flush().unwrap();
+
+        match stdin().read_line(&mut prompt_buf) {
+            Ok(_) => {
+                prompt_buf.pop();
+                if prompt_buf.is_empty() && empty_input_err {
+                    eprintln!("[-] Empty {} !", param_name);
+                    stderr().flush().unwrap();
+                    continue;
+                } else {
+                    return prompt_buf;
+                }
+            }
+
+            Err(error) => {
+                eprintln!(
+                    "[-] Error While Reading {} : {} \n [!] Repeating",
+                    param_name, error
+                );
+                stderr().flush().unwrap();
+                continue;
+            }
+        };
+    }
+}
+
+#[inline(always)]
+pub fn banner() {
+    println!(
+        r#"
+0xCrypt00o Moodle Login - Tool for Getting Moodle Session Quickly and Login Through CLI
+
+    Created By : Eslam Muhammad [0xCrypt00o]
+    Github Repo : https://github.com/Crypt00o/eelu-login
+    Mail : 0xCrypt00o@protonmail.com
+    Support Me On :
+        Bitcoin Address : bc1qdp3f6u3puwkyu6ztxz7hzcurd7jnjyw6hzjcas"#);
 }
